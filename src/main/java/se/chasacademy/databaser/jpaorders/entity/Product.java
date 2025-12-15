fc8bc8ae52b39274086b73d8ea8a5b190fb064fe
rasha knifdi <rasha.knifdi@chasacademy.se>
2025-12-15 15:13:42 +0100

    add Product.java

--- src/main/java/se/chasacademy/databaser/jpaorders/entity/Product.java
+++ src/main/java/se/chasacademy/databaser/jpaorders/entity/Product.java
@@ -1,4 +1,27 @@
 package se.chasacademy.databaser.jpaorders.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "t_product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prod_id")
+    private Long id;
+
+    @Column(name = "prod_name", nullable = false)
+    private String name;
+
+    @Column(name = "prod_desc")
+    private String description;
+
+    @Column(name = "unit_price_cents", nullable = false)
+    private Integer unitPriceCents;
+
+    @Column(name = "active_flag", nullable = false)
+    private String activeFlag;
+
+    // getters/setters
 }
